b87ac91f32c37b7bcc56611076c55030d12b2d4d
MrCageFighter <pedroca.mc2@gmail.com>
2019-10-12 09:02:24 -0300

    main code

---
+++ main.py
@@ -0,0 +1,21 @@
+import copy
+from moves import *
+from pieces import *
+from cubeinput import *
+from algorithms import *
+from ThistlethwaitesAlgorithm import *
+
+print("""1 -> Verde
+2 -> Vermelho
+3 -> Azul
+4 -> Laranja
+5 -> Branco
+6 -> Amarelo
+A orientação sempre é com a face verde para frente e a branca para cima
+""")
+
+Cube = getCube()
+CubeCopy = copy.deepcopy(Cube)
+
+verifyCube(CubeCopy)
+printCube(Cube)
